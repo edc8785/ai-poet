143d00f15080773044c354c2401baa39e5786733
ryong <47944280+edc8785@users.noreply.github.com>
2023-12-26 23:07:08 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+# from dotenv import load_dotenv
+# load_dotenv()
+
+#from langchain.chat_models import ChatOpenAI
+#llm = ChatOpenAI(openai_api_key="")
+
+#from langchain.llms import OpenAI
+#llm = OpenAI()
+#result = llm.predict("hi!")
+#print(result)
+
+#from langchain.chat_models import ChatOpenAI
+#chat_model = ChatOpenAI()
+#result = chat_model.predict("hi!")
+#print(result)
+
+#from langchain.chat_models import ChatOpenAI
+#chat_model = ChatOpenAI()
+
+#content = "코딩"
+#result = chat_model.predict(content + "에 대한 시를 써줘")
+#print(result)
+
+
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+
+chat_model = ChatOpenAI()
+st.title('인공지능 시인')
+
+content = st.text_input('시의 주제를 제시해주세요.')
+
+if st.button('시 작성 요청하기'):
+    with st.spinner('시 작성 중...'):
+        result = chat_model.predict(content + "에 대한 시를 써줘")
+        st.write(result)
+
